80a23ab71ad352bb833cc9bf6c01c5c8fa28aa0a
Rogério <ragnet@gmail.com>
2024-02-06 10:36:44 -0500

    Added secret and .env support

--- README.md
+++ README.md
@@ -5,6 +5,13 @@
 ```
 pip install flask
 pip install mysql-connector-python
+pip install -U setuptools
+pip install python-dotenv
 
 python api.py
+```
+
+```
+contractor
+eyJ2ZXJzaW9uIjoiMi4!1Lj
 ```
--- api.py
+++ api.py
@@ -1,35 +1,59 @@
-import json
+import os
 import mysql.connector
-from flask import Flask, jsonify
 
-# TODO: This configuration should be moved to an environment file
-cnx = mysql.connector.connect(user='', 
-                              password='',
-                              host='127.0.0.1',
-                              database='demo')
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+
+load_dotenv()
+
+API_SECRET = os.getenv('API_SECRET')
 
 app = Flask(__name__)
 
+def get_db_connection():
+    return mysql.connector.connect(
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_DATABASE')
+    )
+
+def check_secret():
+    if request.headers.get('X-Api-Secret') != API_SECRET:
+        raise Exception('Invalid API secret')
+
 #
 # Routes
+#
 # GET /places
 #
 @app.route('/places', methods=['GET'])
 def get_places():
     try:
+        check_secret()
+
+        cnx = get_db_connection()
         cursor = cnx.cursor()
         cursor.execute("SELECT * FROM places")
         places = cursor.fetchall()
 
         return jsonify(places)
     except Exception as e:
-        return jsonify({'error': str(e)})
+        return jsonify({'error': str(e)}), 401
     finally:
         cursor.close()
 
-@app.route('/ping', methods=['GET'])
+#
+# GET /ping
+#
+@app.route('/ping', methods=['GET', 'POST'])
 def ping():
-    return jsonify({'status': 'ok'})
+    try:
+        check_secret()
+
+        return jsonify({'status': 'ok'})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 401
 
 #
 # Run the api server
